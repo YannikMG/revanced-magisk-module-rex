a0e26a0292bf692f4460e6bd8c70f211c920c228
YannikMG <19749617+YannikMG@users.noreply.github.com>
2024-03-31 20:39:44 +0200

    Revert config file changes

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,14 +1,12 @@
 #!/system/bin/sh
-# shellcheck disable=SC2086,SC1091
-. $MODPATH/config
-
+# shellcheck disable=SC2086
 MODDIR=${0%/*}
 RVPATH=$NVBASE/rvhc/${MODDIR##*/}.apk
 
 until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 until [ -d "/sdcard/Android" ]; do sleep 1; done
 while
-	BASEPATH=$(pm path $PKG_NAME)
+	BASEPATH=$(pm path __PKGNAME)
 	svcl=$?
 	[ $svcl = 20 ]
 do sleep 2; done
@@ -23,23 +21,23 @@ if [ $svcl = 0 ]; then
 	BASEPATH=${BASEPATH##*:}
 	BASEPATH=${BASEPATH%/*}
 	if [ -d $BASEPATH/lib ]; then
-		VERSION=$(dumpsys package $PKG_NAME | grep -m1 versionName)
+		VERSION=$(dumpsys package __PKGNAME | grep -m1 versionName)
 		VERSION="${VERSION#*=}"
-		if [ "$VERSION" = $PKG_VER ] || [ -z "$VERSION" ]; then
-			grep $PKG_NAME /proc/mounts | while read -r line; do
+		if [ "$VERSION" = __PKGVER ] || [ -z "$VERSION" ]; then
+			grep __PKGNAME /proc/mounts | while read -r line; do
 				mp=${line#* }
 				mp=${mp%% *}
 				umount -l ${mp%%\\*}
 			done
 			if chcon u:object_r:apk_data_file:s0 $RVPATH; then
 				mount -o bind $RVPATH $BASEPATH/base.apk
-				am force-stop $PKG_NAME
+				am force-stop __PKGNAME
 				[ -f $MODDIR/err ] && mv -f $MODDIR/err $MODDIR/module.prop
 			else
 				err "mount failed"
 			fi
 		else
-			err "version mismatch (installed:${VERSION}, module:$PKG_VER)"
+			err "version mismatch (installed:${VERSION}, module:__PKGVER)"
 		fi
 	else
 		err "zygote crashed"
